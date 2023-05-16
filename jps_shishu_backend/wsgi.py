dbe595c89a1e12d4bd955c1d68d5cd3f6b011fd5
Rushal Barkhade <rushal@e-arth.in>
2023-05-16 23:58:48 +0530

    backend project is created

---
+++ jps_shishu_backend/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for jps_shishu_backend project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'jps_shishu_backend.settings')
+
+application = get_wsgi_application()
